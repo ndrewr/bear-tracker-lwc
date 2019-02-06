37a2641be20dc8c88e0e9c1678a4e97a12e999bf
ndrewr <hello@ndrewr.com>
2019-02-05 18:59:52 -0800

    Add search input to bearList component; Refactor data load based on search term results, implement onchange handler for search input.

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,16 +1,49 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
-
-import getAllBears from '@salesforce/apex/BearController.getAllBears';
-
+/** BearController.searchBears(searchTerm) Apex method */
+import searchBears from '@salesforce/apex/BearController.searchBears';
 export default class BearList extends LightningElement {
-  @wire(getAllBears) bears;
-  appResources = {
-    bearSilhouette: ursusResources + '/img/standing-bear-silhouette.png',
-  }
+	@track searchTerm = '';
+	@wire(searchBears, {searchTerm: '$searchTerm'})
+	bears;
+	appResources = {
+		bearSilhouette: ursusResources +'/img/standing-bear-silhouette.png',
+	};
+	handleSearchTermChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation 	 
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
+	}
+	get hasResults() {
+		return (this.bears.data.length > 0);
+	}
 }
 
+
+//
+// One-line data loading with "wire" implementation below
+//
+// import { LightningElement, wire } from 'lwc';
+// import ursusResources from '@salesforce/resourceUrl/ursus_park';
+
+// import getAllBears from '@salesforce/apex/BearController.getAllBears';
+
+// export default class BearList extends LightningElement {
+//   @wire(getAllBears) bears;
+//   appResources = {
+//     bearSilhouette: ursusResources + '/img/standing-bear-silhouette.png',
+//   }
+// }
+
+//
 // "imperative Apex" implementation below
+//
 // import { LightningElement, track } from 'lwc';
 // import ursusResources from '@salesforce/resourceUrl/ursus_park';
 // // BearController.getAllBears() Apex method
